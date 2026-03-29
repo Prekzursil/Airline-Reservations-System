388cdc0fdf9ee371b85eb1564e01a85c81a01540
Andrei Mihai Visalon <54636077+Prekzursil@users.noreply.github.com>
2026-03-29 02:50:54 +0000

    Remove unused React default imports

--- airline-gui/src/components/SwapSeatsForm.js
+++ airline-gui/src/components/SwapSeatsForm.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import Select from 'react-select';
 import { swapSeats, fetchBookings } from '../services/apiService';
